0b6a911b2b358d07b9648aac65118870be48131c
german <nextphasesoon@mail.ru>
2024-04-22 01:58:03 +0300

    static files

--- api_hummersystems/settings.py
+++ api_hummersystems/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-95=aui$snze92fvpc%yrapdp)062fst03gyii^a!gs)h0z_z(c'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'znxPy.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -126,7 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
